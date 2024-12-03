93a1089d2c67e53ccee5f95ec19a474834ee5948
cosmxr <marcrues@myuax.com>
2024-12-03 11:10:10 +0100

    Initial commit

---
+++ src/main/java/com/example/wakanda_trafico1/WakandaTrafico1Application.java
@@ -0,0 +1,13 @@
+package com.example.wakanda_trafico1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakandaTrafico1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WakandaTrafico1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wakanda_trafico1/WakandaTrafico1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wakanda_trafico1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakandaTrafico1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
