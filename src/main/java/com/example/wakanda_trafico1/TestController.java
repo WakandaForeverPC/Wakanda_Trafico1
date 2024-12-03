7e9cbd9e10f08e2f8504ff5491b63d5fadc33ee1
cosmxr <marcrues@myuax.com>
2024-12-03 11:53:05 +0100

    Conexión con docker establecida

---
+++ src/main/java/com/example/wakanda_trafico1/TestController.java
@@ -0,0 +1,13 @@
+package com.example.wakanda_trafico1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Test successful!";
+    }
+}
